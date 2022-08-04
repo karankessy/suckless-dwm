882690816fd04a5caf97a8f3604956fed2df4b8f
0xErick <54462503+GrillingEvil@users.noreply.github.com>
2022-08-04 12:06:26 +0545

    Add files via upload

---
+++ mouse.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Version 0.1 Tuesday, 07 May 2013
+# Comments and complaints http://www.nicknorton.net
+# GUI for mouse wheel speed using imwheel in Gnome
+# imwheel needs to be installed for this script to work
+# sudo apt-get install imwheel
+# Pretty much hard wired to only use a mouse with
+# left, right and wheel in the middle.
+# If you have a mouse with complications or special needs,
+# use the command xev to find what your wheel does.
+#
+### see if imwheel config exists, if not create it ###
+if [ ! -f ~/.imwheelrc ]
+then
+cat >~/.imwheelrc<<EOF
+".*"
+None,      Up,   Button4, 4
+None,      Down, Button5, 4
+Control_L, Up,   Control_L|Button4
+Control_L, Down, Control_L|Button5
+Shift_L,   Up,   Shift_L|Button4
+Shift_L,   Down, Shift_L|Button5
+
+EOF
+fi
+
+
+
